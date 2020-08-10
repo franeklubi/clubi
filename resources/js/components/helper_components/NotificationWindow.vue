5cbd35418f3b27f9a15e7d8f9e54c0be2bb5a286
franek <franeklubi@gmail.com>
2020-08-10 16:05:15 +0200

    Show fetched notifications in notification box

--- resources/js/components/helper_components/NotificationWindow.vue
+++ resources/js/components/helper_components/NotificationWindow.vue
@@ -1,14 +1,31 @@
 <template>
     <div class="notifications">
-        <div class="button">
+        <div class="button" @click="toggleOpened">
             <i class="far fa-envelope"></i>
             <span class="badge badge-danger unseen-count"
                 v-if="unseen_count != 0"
             >{{ unseen_count }}</span>
         </div>
-        <div class="box">
-            <div class="">
-
+        <div class="box box-position" :class="{'invisible': !box_opened}">
+            <div class="card">
+                <div class="card-header">
+                    Notifications
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item" v-if="feedback">
+                        <div class="alert alert-danger">
+                            {{ feedback }}
+                        </div>
+                    </li>
+                    <li class="list-group-item"
+                        v-for="notification in notifications"
+                        :key="notification.id"
+                    >
+                        <a :href="notification.link">
+                            {{ notification.message }}
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -26,10 +43,22 @@
                 next_page_url: '',
                 unseen_count: 0,
                 url: '/notifications',
+                box_opened: false,
+                fetched: false,
+                feedback: '',
             }
         },
 
         methods: {
+            toggleOpened() {
+                this.box_opened = !this.box_opened;
+
+                if ( !this.fetched ) {
+                    this.getNotifications();
+                    this.fetched = true;
+                }
+            },
+
             getUnseenCount() {
                 let get_url = this.url + '/count';
 
@@ -42,7 +71,8 @@
 
             getNotifications() {
                 axios.get(this.url).then((res) => {
-                    this.notificatinos = res.data.notificatinos;
+                    this.next_page_url = res.data.notifications.next_page_url;
+                    this.notifications = res.data.notifications.data;
                 }).catch((err) => {
                     this.feedback = this.handleAxiosError(err);
                 });
@@ -71,11 +101,38 @@
         justify-content: center;
     }
 
+    .button {
+        cursor: pointer;
+    }
+
     .unseen-count {
         position: absolute;
         top: 0;
         left: 1.4rem;
 
         font-size: 0.8rem;
+    }
+
+    .box {
+        font-size: 1rem;
+
+        z-index: 2;
+
+        width: 30vw;
+
+        position: absolute;
+        top: 2.3rem;
+    }
+
+    .box-position {
+        right: 0;
+    }
+
+    @media (max-width: 768px) {
+        .box-position {
+            left: 0;
+
+            width: 90vw;
+        }
     }
 </style>
