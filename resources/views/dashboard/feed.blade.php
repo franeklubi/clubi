0f0675cbcea8440c1774f2d0e01e040771b96fa6
franek <franeklubi@gmail.com>
2020-08-12 11:46:56 +0200

    Remove padding from dashboard.feed container

--- resources/views/dashboard/feed.blade.php
+++ resources/views/dashboard/feed.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container p-0">
     @guest
         <div class="row justify-content-center">
             <div class="col-lg-8">
