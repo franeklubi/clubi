b72cbc95b17b6c80b3d899206641b9b2de1df386
franek <franeklubi@gmail.com>
2020-09-26 21:45:01 +0200

    Add laravel/sanctum

--- app/User.php
+++ app/User.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
