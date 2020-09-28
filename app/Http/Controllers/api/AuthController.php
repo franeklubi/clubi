5b728c388bf1314e3c9d4f0a5b3c6376113d84e6
franek <franeklubi@gmail.com>
2020-09-28 23:02:34 +0200

    Add register api route

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -4,29 +4,54 @@ namespace App\Http\Controllers\api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Controllers\Controller;
 use Illuminate\Validation\ValidationException;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Auth\RegisterController;
 
 
 class AuthController extends Controller
 {
 
-	public function login(Request $request)
-	{
-	    $request->validate([
-	        'email' => ['required', 'string', 'email'],
-	        'password' => ['required', 'string', 'min:8'],
-	        'device_name' => ['required', 'string', 'max:256'],
-	    ]);
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string', 'min:8'],
+            'device_name' => ['required', 'string', 'max:256'],
+        ]);
 
-	    $user = \App\User::where('email', $request->email)->first();
+        $user = \App\User::where('email', $request->email)->first();
 
-	    if (! $user || ! Hash::check($request->password, $user->password)) {
-	        throw ValidationException::withMessages([
-	            'email' => ['The provided credentials are incorrect.'],
-	        ]);
-	    }
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
 
-	    return $user->createToken($request->device_name)->plainTextToken;
-	}
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = app(RegisterController::class)->validator($request->all());
+
+        if($validator->fails()){
+            return response()->json($validator->messages(), );
+        }
+
+        $user = app(RegisterController::class)->create($request->all());
+
+        $token = $user->createToken('register_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', 'AuthController@login');
+
+Route::post('/register', 'AuthController@register');
