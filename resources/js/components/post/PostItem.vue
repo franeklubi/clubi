dfc3fdeba4ccc228e10afa5da9c48a51af5a2b5f
Filip Majiroš <filip.majiros@student.tuke.sk>
2020-11-08 21:41:21 +0100

    get number of likes.

--- resources/js/components/post/PostItem.vue
+++ resources/js/components/post/PostItem.vue
@@ -52,7 +52,7 @@
                         @click="toggleLike"
                         :disabled="!is_member"
                     >
-                        <span class="font-weight-bold">{{ likes.length }}</span>
+                        <span class="font-weight-bold">{{ likes }}</span>
                         <span :class="likeIconClass"></span>
                     </button>
                 </div>
@@ -88,10 +88,14 @@
                     +'/posts/'+this.post.id,
                 likes_link: '/groups/'+this.post.group.id_string
                     +'/posts/'+this.post.id+'/likes',
+                like_users_link: '/groups/'+this.post.group.id_string
+                    +'/posts/'+this.post.id+'/likes/users',
                 owner: this.post.group.owner,
-                likes: this.post.likes,
+                likes: this.post.like_count,
                 feedback: '',
                 char_cutoff: parseInt(process.env.MIX_READ_MORE_CHAR_CUTOFF),
+                like_users: this.post.likes,
+                userLiked: false
             }
         },
 
@@ -118,16 +122,27 @@
                 })
             },
 
+            loadLikeUsers() {
+                axios.get(this.like_users_link).then((res) => {
+                    if (res.data.likes) {
+                        let index = res.data.likes.findIndex((like) => {
+                            return like.user_id == this.user_id;
+                        });
+                        this.userLiked = index ? false : true;
+                    }
+                }).catch((err) => {
+                    this.feedback = this.handleAxiosError(err);
+                })
+            },
+
             toggleLike() {
                 axios.post(this.likes_link).then((res) => {
                     if ( res.data.state == 'liked' ) {
-                        this.likes.push(res.data.like);
+                        this.likes = this.likes + 1;
+                        this.userLiked = true;
                     } else {
-                        let index = this.likes.findIndex((like) => {
-                            return like.id == res.data.like.id;
-                        });
-
-                        this.likes.splice(index, 1);
+                        this.likes = this.likes - 1;
+                        this.userLiked = false;
                     }
                 }).catch((err) => {
                     this.feedback = this.handleAxiosError(err);
@@ -137,11 +152,7 @@
 
         computed: {
             isLiked() {
-                let index = this.likes.findIndex((like) => {
-                    return like.user_id == this.user_id;
-                });
-
-                return index>-1;
+                return this.userLiked;
             },
 
             likeIconClass() {
@@ -157,6 +168,8 @@
             if ( typeof this.post.likes == 'undefined' ) {
                 this.loadLikes();
             }
+
+            this.loadLikeUsers();
         }
     }
 </script>
